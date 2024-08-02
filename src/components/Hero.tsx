ba671352df08a027173a5b28895dc492ce224d20
sid <sidengineer.014@gmail.com>
2024-08-02 23:44:37 +0530

    animations added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,50 +6,66 @@ import { ArrowRight, LogIn } from "lucide-react"
 import SubscribeButton from "./SubscribeButton"
 import Link from "next/link"
 import { Cursor, useTypewriter } from "react-simple-typewriter"
+import {motion } from "framer-motion"
+import Image from "next/image"
 
 
 type Props = {
-    isPro : boolean,
-    firstChat : { id: number; userId: string; pdfName: string; pdfUrl: string; createdAt: Date; fileKey: string; } | undefined ;
-    isAuth : boolean
+  isPro: boolean,
+  firstChat: { id: number; userId: string; pdfName: string; pdfUrl: string; createdAt: Date; fileKey: string; } | undefined;
+  isAuth: boolean
 }
 
-const Hero = ({isPro , firstChat , isAuth}: Props) => {
+const Hero = ({ isPro, firstChat, isAuth }: Props) => {
 
-    const [text] = useTypewriter({
-        words : ["Connecting..." , "you with" , "your documents" , "seamlessly" ],
-        loop : false,
-        typeSpeed : 120,
-        deleteSpeed : 50
-      })
+  const [text] = useTypewriter({
+    words: ["Connecting...", "you with", "your documents", "seamlessly"],
+    loop: false,
+    typeSpeed: 120,
+    deleteSpeed: 50
+  })
 
   return (
-     
-    <section className="flex flex-col justify-center items-center mt-10 space-y-5  " >
-          <div className="flex flex-col justify-center items-center space-y-4" >
-            <div className="flex justify-center items-center gap-2">
-              <h1 className="text-4xl max-sm:text-2xl text-black font-bold " >PDFs that Talk Back!</h1>
-              <UserButton afterSignOutUrl="/" />
-            </div>
-            {!isAuth ? <div className="max-sm:flex-col flex justify-center items-center gap-2" >
 
-              <Button className="h-8 font-semibold " disabled={!firstChat} >
-                Go to chats
-                <span>
-                  <ArrowRight className="w-5 h-5 pl-1" />
-                </span>
-              </Button>
+    <section className=" relative flex flex-col justify-center items-center mt-10 space-y-5  " >
+      <div className="flex flex-col justify-center items-center space-y-4" >
+        <div className="flex justify-center items-center gap-2">
+          <h1 className="text-4xl max-sm:text-2xl text-black font-bold " >PDFs that Talk Back!</h1>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+        {!isAuth ? <div className="max-sm:flex-col flex justify-center items-center gap-2" >
 
-              <SubscribeButton isPro={isPro} />
-            </div> : <Link href="/sign-in" > <Button className="flex justify-center items-center gap-2" > Login to get started <span><LogIn className="w-4 h-4" /></span></Button></Link>}
-          </div>
-          <div className="text-lg max-w-[90%] sm:max-w-[50%] ">
-            {/* <p className="text-center" >Engage with your PDFs like never before. Ask questions, get instant answers, and transform your documents into interactive dialogues. Streamline your workflow with <span className="font-bold text-3xl text-blue-600" >{text}</span>. */}
-            <p className="text-center" >Engage with your PDFs like never before. Ask questions, get instant answers, and transform your documents into interactive dialogues. Streamline your workflow with <span className="text-3xl"  style={{ color: '#D3D3D3', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }} >{text}</span>
-            <span> <Cursor cursorStyle= "|" /> </span>
-            </p>
-          </div>
-        </section>
+          <Button className="h-8 font-semibold " disabled={!firstChat} >
+            Go to chats
+            <span>
+              <ArrowRight className="w-5 h-5 pl-1" />
+            </span>
+          </Button>
+
+          <SubscribeButton isPro={isPro} />
+        </div> : <Link href="/sign-in" > <Button className="flex justify-center items-center gap-2" > Login to get started <span><LogIn className="w-4 h-4" /></span></Button></Link>}
+      </div>
+      <div className="text-lg max-w-[90%] sm:max-w-[50%] ">
+        {/* <p className="text-center" >Engage with your PDFs like never before. Ask questions, get instant answers, and transform your documents into interactive dialogues. Streamline your workflow with <span className="font-bold text-3xl text-blue-600" >{text}</span>. */}
+        <p className="text-center" >Engage with your PDFs like never before. Ask questions, get instant answers, and transform your documents into interactive dialogues. Streamline your workflow with <span className="text-3xl" style={{ color: '#D3D3D3', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }} >{text}</span>
+          <span> <Cursor cursorStyle="|" /> </span>
+        </p>
+      </div>
+
+      <motion.div className="absolute top-0 lg:right-10 md:right-6 flex max-sm:hidden" animate={{
+        translateY: [-20, 20]
+      }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 3,
+          ease: "easeInOut",
+
+        }}
+      >
+        <Image src="/star.png" alt="star" width={200} height={200} quality={100} />
+      </motion.div>
+    </section>
   )
 }
 
--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,14 +1,55 @@
+"use client"
+
 import Image from "next/image"
+import { useRef } from "react"
+
+import { useScroll, useTransform, motion } from "framer-motion"
 
 
 
 const Process = () => {
+
+    const processRef = useRef(null)
+
+    const { scrollYProgress } = useScroll({
+        target: processRef,
+        offset: ["start end", "end start"]
+    })
+
+    const translateY = useTransform(scrollYProgress, [0, 1], [-150, 150])
+
+
+    const FadeInAnimationVarient = {
+        initial: {
+            opacity: 0,
+            y: 100
+        },
+
+        animate: (custom: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: custom * 0.10
+            }
+        })
+    }
+
+
     return (
-        <section className="w-full h-fit pt-20" >
+        <motion.section ref={processRef} className=" relative w-full h-fit pt-20" >
             <div className="flex max-sm:flex-col justify-center items-center gap-6 z-10 " >
-                <div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 " >
+                <motion.div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 "
+                    variants={FadeInAnimationVarient}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{
+                        once: true,
+                    }}
+                    custom={0}
+
+                >
                     <div className="flex flex-col justify-center items-center space-y-3" >
-                        <Image src="/upload.png" alt="demo" width={300} height={200} quality={100} className="object-cover rounded-lg"/>
+                        <Image src="/upload.png" alt="demo" width={300} height={200} quality={100} className="object-cover rounded-lg" />
 
                         <div className="bg-black text-white ring-1 rounded-full w-10 h-10 flex justify-center items-center font-bold font-serif text-lg" >
                             1
@@ -18,8 +59,16 @@ const Process = () => {
                         </div>
                     </div>
 
-                </div>
-                <div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 " >
+                </motion.div>
+                <motion.div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 "
+                    variants={FadeInAnimationVarient}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{
+                        once: true,
+                    }}
+                    custom={1}
+                >
                     <div className="flex flex-col justify-center items-center space-y-3" >
                         <Image src="/documents.png" alt="demo" width={300} height={200} quality={100} className="object-cover rounded-lg" />
 
@@ -31,12 +80,21 @@ const Process = () => {
                         </div>
                     </div>
 
-                </div>
-                <div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 " >
+                </motion.div>
+                <motion.div className="h-fit max-w-64 p-3 rounded-lg  bg-slate-200  hover:ring-1 ring-sky-900 hover:hue-rotate-30 hover:scale-105 "
+
+                    variants={FadeInAnimationVarient}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{
+                        once: true,
+                    }}
+                    custom={2}
+                >
                     <div className="flex flex-col justify-center items-center space-y-3" >
-                        
-                            <Image src="/upload.png" alt="demo" width={300} height={200} className=" rounded-lg" />
-                        
+
+                        <Image src="/upload.png" alt="demo" width={300} height={200} className=" rounded-lg" />
+
 
                         <div className="bg-black text-white ring-1 rounded-full w-10 h-10 flex justify-center items-center font-bold font-serif text-lg" >
                             3
@@ -46,11 +104,31 @@ const Process = () => {
                         </div>
                     </div>
 
-                </div>
+                </motion.div>
 
 
             </div>
-        </section>
+
+            <motion.div className="absolute top-0 lg:left-10 md:left-6 flex max-sm:hidden"
+                style={{
+                    rotate: 30,
+                    translateY: translateY
+                }}
+            >
+                <Image src="/cylinder.png" alt="cylinder" width={200} height={200} quality={100} />
+            </motion.div>
+
+            <motion.div className="absolute bottom-0 lg:right-10 md:right-6 flex max-sm:hidden"
+                style={{
+                    rotate: 30,
+                    translateY: translateY
+                }}
+            >
+                <Image src="/noodle.png" alt="noodle" width={200} height={200} quality={100} />
+            </motion.div>
+
+
+        </motion.section >
     )
 }
 
